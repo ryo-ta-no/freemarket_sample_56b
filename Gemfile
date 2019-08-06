b7d09dedef8d2064469d6a14a350e791024bcb39
potter9610 <potterqaz@gmail.com>
2019-08-06 19:31:03 +0900

    多階層カテゴリー作成

--- Gemfile
+++ Gemfile
@@ -77,6 +77,7 @@ gem 'carrierwave'
 gem 'mini_magick'
 gem 'devise'
 gem 'active_hash'
+gem 'ancestry'
 group :production do
   gem 'unicorn', '5.4.1'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_06_062807) do
+ActiveRecord::Schema.define(version: 2019_08_06_080048) do
+
+  create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "category_name", null: false
+    t.string "ancestry"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "prefectures", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.datetime "created_at", null: false
