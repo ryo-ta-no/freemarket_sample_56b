651f55f88b476b8089bc716d13a6264e6f63fa03
ueda-takahiro <ueda196@gmail.com>
2019-08-08 20:50:18 +0900

    本人情報登録ページ　　
    itemsコントローラーいじってます。
    コード量確認、
    クラスの一意性確認おねがします。

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,6 +3,14 @@ class ItemsController < ApplicationController
     
   end
   def show
+    @search_params = user_search_params
+    @prefecrure = Prefecture.all
   end
   
+
+private
+
+  def user_search_params
+    params.fetch(:search, {}).permit(:id,:prefecture)
+  end
 end
