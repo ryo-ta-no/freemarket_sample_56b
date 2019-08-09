cba4caefb271ffe8d298bed38008eba1ac39b51e
potter9610 <potterqaz@gmail.com>
2019-08-09 17:32:57 +0900

    商品状態

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,6 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
+  enum state: [['---',""], ['新品、未使用', 1], ['未使用に近い', 2],['目立った傷や汚れなし', 3], ['やや傷や汚れあり', 4], ['傷や汚れあり', 5], ['全体的に状態が悪い', 6]]
   belongs_to_active_hash :prefecture
   has_many :phone
   belongs_to :user
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_07_054736) do
+ActiveRecord::Schema.define(version: 2019_08_09_074804) do
 
   create_table "brands", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "brand", null: false
@@ -39,7 +39,7 @@ ActiveRecord::Schema.define(version: 2019_08_07_054736) do
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
     t.text "explain", null: false
-    t.string "state", null: false
+    t.integer "state", null: false
     t.integer "price", null: false
     t.integer "buyer_id", null: false
     t.integer "seller_id", null: false
