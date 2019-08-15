f54a94c713656feee1ba6a6319a1f9aa6ddff34e
ryo-ta-no <kabhi4w@gmail.com>
2019-08-15 15:27:51 +0900

    簡単な商品検索機能実装

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,5 +1,17 @@
 class SearchesController < ApplicationController
-  def index
-    
+
+  def search
+    @parents = Category.all.order("id ASC").limit(13)
+              # Category.where(ancestry: nil)
+    #Viewのformで取得したパラメータをモデルに渡す
+    # @items = Item.search(params[:search])
+    @items = Item.where('name LIKE(?)', "%#{params[:keyword]}%").order("id DESC").page(params[:page]).per(15)
+    # if params[:keyword] == ""
+    #   redirect_to '/items/search?utf8=✓&keyword=+++'
+    # end
+    # if @items.count == 0
+    #   @all_items = Item.limit(25).order("id ASC")
+    # end
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,11 @@ Rails.application.routes.draw do
 
   resources :ragistrations
 
-  resources :searches
+  resources :searches do
+    collection do
+      get 'search'
+    end
+  end
+
 
 end
