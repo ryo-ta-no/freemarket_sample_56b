016c53ad97ddfb423768d733a8d71b8db43ac2b1
potter9610 <potterqaz@gmail.com>
2019-08-16 15:16:14 +0900

    s3

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -7,11 +7,10 @@ CarrierWave.configure do |config|
     config.storage :fog
     config.fog_provider = 'fog/aws'
     config.fog_directory = 'mercari56b'
-    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/mercari56b'
     config.fog_credentials = {
       provider: 'AWS',
-      aws_access_key_id: Rails.application.credentials.aws_access_key_id,
-      aws_secret_access_key: Rails.application.credentials.aws_secret_access_key,
+      aws_access_key_id: Rails.application.credentials.aws[:access_key_id],
+      aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],
       region: 'ap-northeast-1',
     }
   else
