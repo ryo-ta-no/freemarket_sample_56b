2cb630721ef14009edaac52ae0d8d4e69959d15a
ryo-ta-no <kabhi4w@gmail.com>
2019-08-17 15:36:45 +0900

    viewマークアップ完了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,4 +12,11 @@ class UsersController < ApplicationController
   def goods_detail
   end
 
+  def destroy
+    @item = Item.find(id: params[:id])
+    @item.destroy
+    redirect_to("/")
+    # 削除されるとそのページはなくなるので、違うページに遷移する
+  end
+
 end
