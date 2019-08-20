5df4145fa4fbe6a32c95ff9d6f125c35506a0519
potter9610 <potterqaz@gmail.com>
2019-08-20 14:55:12 +0900

    画像

--- app/assets/javascripts/photo.js
+++ app/assets/javascripts/photo.js
@@ -1,26 +1,42 @@
 $(function(){
-$('#image').on('change', function(e){
-  var file = e.target.files[0];
-  console.log(file)
-  var reader = new FileReader();
-  reader.onload = (function(e){
-    var ar = new Uint8Array(reader.result);  
-    $(".photo_display").append($("<img class = photo>").attr("src", e.target.result));
-})
-reader.readAsDataURL(file);
-$('#image').css({'display':'none'});
-console.log(1)
-$('#image2').css('display','');
-console
-})
+  
+  $('.file_field').on('change', function(e){
+    var file = e.target.files[0];
+    var reader = new FileReader();
+    reader.onload = (function(e){
+      var ar = new Uint8Array(reader.result);  
+      $(".photo_display").append($("<img class = photo_img>").attr("src", e.target.result));
+  })
+  reader.readAsDataURL(file);
 
-$('#image2').on('change', function(e){
-  var file = e.target.files[0];
-  console.log(file)
-  var reader = new FileReader();
-  reader.onload = (function(e){
-    var ar = new Uint8Array(reader.result);  
-    $(".photo_display").append($("<img class = photo>").attr("src", e.target.result));
-})
-})
+  var num = $('.photo_display img').length;
+  console.log(num)
+  if (num == 0){
+  $('#photo').css({'display':'none'});
+  $('#photo2').css('display','');
+  }
+  else if (num == 1){
+    $('#photo2').css({'display':'none'});
+    $('#photo3').css('display','');
+  }
+  else if (num == 2){
+    $('#photo3').css({'display':'none'});
+    $('#photo4').css('display','');
+  }
+  else if (num == 3){
+    $('#photo4').css({'display':'none'});
+    $('#photo5').css('display','');
+  }
+  else if (num == 4){
+    $('#photo5').css({'display':'none'});
+    $('#photo6').css('display','');
+ 
+  }
+  else if (num == 5){
+    $('.image_label').css({'display':'none'});
+
+ 
+  }
+  })
+
 })
