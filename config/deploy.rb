39ab9da95f71eea245b01670cad8e18e454c21bd
potter9610 <potterqaz@gmail.com>
2019-08-20 17:50:23 +0900

    修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', '
 set :rbenv_type, :user
 set :rbenv_ruby, '2.5.1'
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['/.ssh/potter2862.pem']  
+                  keys: ['~/.ssh/potter2862.pem']  
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
@@ -24,6 +24,6 @@ set :default_env, {
   AWS_ACCESS_KEY_ID: ENV["AWS_ACCESS_KEY_ID"],
   AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"],
   BASIC_AUTH_USER: ENV["BASIC_AUTH_USER"],
-  BASIC_AUTH_PASSWORD: ENV["BASIC_AUTH_PASSWORD"],
+  BASIC_AUTH_PASSWORD: ENV["BASIC_AUTH_PASSWORD"]
 }
 set :linked_files, fetch(:linked_files, []).push("config/master.key")
