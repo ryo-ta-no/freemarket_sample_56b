36d7985c2dc25b0ffb7fa2fdd06da4de931e416b
potter9610 <potterqaz@gmail.com>
2019-08-20 18:10:56 +0900

    削除ボタン追加

--- app/assets/javascripts/photo.js
+++ app/assets/javascripts/photo.js
@@ -13,28 +13,39 @@ $(function(){
   if (num == 0){
   $('#photo').css({'display':'none'});
   $('#photo2').css('display','');
+  $('.remove_btn1').css('display','');
   }
   else if (num == 1){
     $('#photo2').css({'display':'none'});
     $('#photo3').css('display','');
+    $('.remove_btn2').css('display','');
   }
   else if (num == 2){
     $('#photo3').css({'display':'none'});
     $('#photo4').css('display','');
+    $('.remove_btn3').css('display','');
   }
   else if (num == 3){
     $('#photo4').css({'display':'none'});
     $('#photo5').css('display','');
+    $('.remove_btn4').css('display','');
   }
   else if (num == 4){
     $('#photo5').css({'display':'none'});
     $('#photo6').css('display','');
+    $('.remove_btn5').css('display','');
  
   }
   else if (num == 5){
     $('.image_label').css({'display':'none'});
+    $('.remove_btn6').css('display','');
 
   }
 
   })
+
+
+
+
+
 })
