990c4b540cccfb2decf3e20111436b996d9b468f
potter9610 <potterqaz@gmail.com>
2019-08-22 18:10:52 +0900

    お

--- app/assets/javascripts/category_drop.js
+++ app/assets/javascripts/category_drop.js
@@ -1,6 +1,5 @@
 $(function() {
-	$('#category_drop').hover(
-			function() {
+	$('#category_drop').hover(function() {
         $('#category_drop').css('color', '#0099e8');
       console.log(1)
         $.ajax({
@@ -9,13 +8,14 @@ $(function() {
           dataType: 'json',
         })
         console.log(2)
-        done(function(parent){
-          insertHTML = '';
-          parent.forEach(function(parents){
-            insertHTML += parents;
-            category_drop_parent(insertHTML);
-          })
-        })
+        // done(function(parent){
+        //   console.log(3)
+        //   insertHTML = '';
+        //   parent.forEach(function(parents){
+        //   insertHTML += parents;
+        //   category_drop_parent(insertHTML);
+        //   })
+        // })
 			},
 			function() {
 				$('#category_drop').css('color', '#333');
