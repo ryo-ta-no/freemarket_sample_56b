ef426899140dbd1c1841afe56d138b8a5fb38839
potter9610 <potterqaz@gmail.com>
2019-08-22 18:14:10 +0900

    修正

---
+++ app/assets/javascripts/category_drop.js
@@ -0,0 +1,9 @@
+$(function() {
+	$('#category_drop').hover(function() {
+        $('#category_drop').css('color', '#0099e8');
+			},
+			function() {
+				$('#category_drop').css('color', '#333');
+			}
+	);
+});
