34b6c7e853eab1e15a6d46174b7a95fe8238647a
ueda-takahiro <ueda196@gmail.com>
2019-08-27 20:11:22 +0900

    nicknameにindex貼れない問題解消

--- db/migrate/20190807035152_add_column_to_users.rb
+++ db/migrate/20190807035152_add_column_to_users.rb
@@ -1,7 +1,5 @@
 class AddColumnToUsers < ActiveRecord::Migration[5.2]
   def change
-    add_column :users, :user_id, :string, foreign_key: true
-    add_column :users, :nickname, :string, null: false, index: true
     add_column :users, :first_name, :string, null: false
     add_column :users, :last_name, :string, null: false
     add_column :users, :first_kana, :string, null: false
