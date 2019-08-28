64ba9aad4b56e8655939e3430eb92d475a39e4cd
ryo-ta-no <kabhi4w@gmail.com>
2019-08-28 15:32:25 +0900

    簡単な商品検索機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,6 +7,17 @@ class ItemsController < ApplicationController
 
   end
 
+  def search
+    @parents = Category.all.order("id ASC").limit(13)
+    @items = Item.where('name LIKE(?)', "%#{params[:keyword]}%").order("id DESC").page(params[:page]).per(15)
+    if params[:keyword] == ""
+      redirect_to '/items/search?utf8=✓&keyword=+++'
+    end
+    if @items.count == 0
+      @all_items = Item.limit(25).order("id ASC")
+    end
+  end
+
   def edit
     @item = Item.find(21)
     @photo = @item.photos
